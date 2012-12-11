3189902944ca225d77584c4860bb6bdff760f02a
Liviu Cristian Mirea-Ghiban <contact@liviucmg.com>
2012-12-11 13:29:52 +0200

    ROBO AI Code Challange. Commit at 05:00, day 2.

--- octeta/octeta.ino
+++ octeta/octeta.ino
@@ -7,6 +7,7 @@ int MOTOR2_PIN2 = 9;
 struct movement {
   int mode;
   int ticks;
+  int priority;
 };
 
 void setup()
@@ -28,17 +29,44 @@ int
   colorThresholdFata = 600,
   colorThresholdSpate = 750,
   distThreshold = 350,
-  ignoreWhiteTicks = 0;
+  ignoreWhiteTicks = 0,
+  defaultModeTicks = 0;
 
 const int
   MOVE_MODE_FATA = 0,
   MOVE_MODE_STANGA = 1,
   MOVE_MODE_DREAPTA = 2,
-  MOVE_MODE_SPATE = 3;
+  MOVE_MODE_SPATE = 3,
+  MOVE_MODE_STOP = 4,
+  MOVE_MODE_SPATE_STANGA = 5,
+  MOVE_MODE_SPATE_DREAPTA = 6;
   
 const int
   ROTATE_TICKS = 200,
-  IGNORE_WHITE_TICKS = 100;
+  DISTANCE_M = 2;
+
+int getCurPriority() {
+  if (moves == 0) {
+    return 9999;
+  }
+  return m[0].priority;
+}
+
+void addMove(int mode, int ticks, int priority) {
+  // Mutari cu prioritate mai mica? Screw them.
+  while (
+    moves > 0 &&
+    m[moves - 1].priority > priority
+  ) {
+    moves--;
+  }
+  
+  // Adauga miscare
+  m[moves].mode = mode;
+  m[moves].ticks = ticks;
+  m[moves].priority = priority;
+  moves++;
+}
 
 void loop()
 {
@@ -47,178 +75,98 @@ void loop()
   int S_SS = analogRead(3); // spate stanga // lat dr
   int S_SD = analogRead(2); // spate dreapta // lat stg
   int dist = analogRead(4); // distanta
-  /*
-  Serial.print(S_FS);
-  Serial.print(" ");
-  Serial.print(S_FD);
-  Serial.print(" ");
-  Serial.print(S_SS);
-  Serial.print(" ");
-  Serial.print(S_SD);
-  Serial.println("");
-  */
-  if (true || moves == 0)  {    
-//  if (ignoreWhiteTicks == 0) {
+  
+  if (getCurPriority() > 2) {
     if (
       S_SS < colorThresholdSpate &&
       S_SD < colorThresholdSpate
     ) {
-        Serial.println("Spate doua;");        
-        moves = 0;
-        m[moves].mode = MOVE_MODE_DREAPTA;
-        m[moves].ticks = ROTATE_TICKS * 4;
-        moves++;
-        m[moves].mode = MOVE_MODE_FATA;
-        m[moves].ticks = ROTATE_TICKS * 2;
-        moves++;    
-        ignoreWhiteTicks = IGNORE_WHITE_TICKS;
+      addMove(MOVE_MODE_FATA    , ROTATE_TICKS * 4 * DISTANCE_M, 2);
+      addMove(MOVE_MODE_DREAPTA , ROTATE_TICKS * 2, 2);
+      Serial.println("Spate doua;");
     }   
     else if (
       S_FS < colorThresholdFata &&
       S_FD < colorThresholdFata
     ) {
-        Serial.println("Fata doua;");        
-        moves = 0;
-        m[moves].mode = MOVE_MODE_DREAPTA;
-        m[moves].ticks = ROTATE_TICKS * 4;
-        moves++;
-        m[moves].mode = MOVE_MODE_FATA;
-        m[moves].ticks = ROTATE_TICKS * 2;
-        moves++;    
-        ignoreWhiteTicks = IGNORE_WHITE_TICKS;
+      addMove(MOVE_MODE_SPATE , ROTATE_TICKS * 4 * DISTANCE_M,  2);
+      addMove(MOVE_MODE_STANGA , ROTATE_TICKS * 2, 2);
+      Serial.println("Fata doua;");    
     }
     else if (
       S_FS < colorThresholdFata &&
       S_SS < colorThresholdSpate
     ) {
-        Serial.println("Stanga doua;");        
-        moves = 0;
-        m[moves].mode = MOVE_MODE_DREAPTA;
-        m[moves].ticks = ROTATE_TICKS;
-        moves++;
-        m[moves].mode = MOVE_MODE_FATA;
-        m[moves].ticks = ROTATE_TICKS * 2;
-        moves++;    
-        ignoreWhiteTicks = IGNORE_WHITE_TICKS;
+      addMove(MOVE_MODE_DREAPTA , ROTATE_TICKS    , 2);
+      addMove(MOVE_MODE_FATA    , ROTATE_TICKS * 4 * DISTANCE_M, 2);
+      Serial.println("Stanga doua;"); 
     }
     else if (
       S_FD < colorThresholdFata &&
       S_SD < colorThresholdSpate
     ) {
-        Serial.println("Dreapta doua;");        
-        moves = 0;
-        m[moves].mode = MOVE_MODE_STANGA;
-        m[moves].ticks = ROTATE_TICKS;
-        moves++;
-        m[moves].mode = MOVE_MODE_FATA;
-        m[moves].ticks = ROTATE_TICKS * 2;
-        moves++;    
-        ignoreWhiteTicks = IGNORE_WHITE_TICKS;
+      addMove(MOVE_MODE_STANGA  , ROTATE_TICKS    , 2);
+      addMove(MOVE_MODE_FATA    , ROTATE_TICKS * 4 * DISTANCE_M, 2);
+      Serial.println("Dreapta doua;"); 
     }
-    else if (S_FS < colorThresholdFata) { // E alb
+  }
+  
+  if (getCurPriority() > 5) {
+    if (S_FS < colorThresholdFata) { // E alb
       if (dist > distThreshold) { // Are ceva in fata
-        moves = 0;
+        addMove(MOVE_MODE_STANGA  , ROTATE_TICKS    , 5);
+        addMove(MOVE_MODE_SPATE   , ROTATE_TICKS * 2 * DISTANCE_M, 5);
         Serial.println("Fata stanga; il vad in fata;");
-        m[moves].mode = MOVE_MODE_STANGA;
-        m[moves].ticks = ROTATE_TICKS;
-        moves++;
-        m[moves].mode = MOVE_MODE_SPATE;
-        m[moves].ticks = ROTATE_TICKS * 2;
-        moves++;
-        ignoreWhiteTicks = IGNORE_WHITE_TICKS;
       }
       else {
-        moves = 0;
+        addMove(MOVE_MODE_SPATE  , ROTATE_TICKS * 2 * DISTANCE_M   , 5);
+        addMove(MOVE_MODE_DREAPTA, ROTATE_TICKS       , 5);
         Serial.println("Fata stanga;");
-        m[moves].mode = MOVE_MODE_SPATE;
-        m[moves].ticks = ROTATE_TICKS * 2;
-        moves++;
-        m[moves].mode = MOVE_MODE_DREAPTA;
-        m[moves].ticks = ROTATE_TICKS;
-        moves++;
-        ignoreWhiteTicks = IGNORE_WHITE_TICKS;
       }
     }
     else if (S_FD < colorThresholdFata) { // E alb
       if (dist > distThreshold) { // Are ceva in fata
-        moves = 0;
+        addMove(MOVE_MODE_DREAPTA  , ROTATE_TICKS     , 5);
+        addMove(MOVE_MODE_SPATE,     ROTATE_TICKS * 2 * DISTANCE_M , 5);
         Serial.println("Fata dreapta; il vad in fata;");
-        m[moves].mode = MOVE_MODE_DREAPTA;
-        m[moves].ticks = ROTATE_TICKS;
-        moves++;
-        m[moves].mode = MOVE_MODE_SPATE;
-        m[moves].ticks = ROTATE_TICKS * 2;
-        moves++;
-        ignoreWhiteTicks = IGNORE_WHITE_TICKS;
       }
       else {
-        moves = 0;
+        addMove(MOVE_MODE_SPATE  , ROTATE_TICKS * 2   , 5);
+        addMove(MOVE_MODE_STANGA , ROTATE_TICKS       , 5);
         Serial.println("Fata dreapta;");
-        m[moves].mode = MOVE_MODE_SPATE;
-        m[moves].ticks = ROTATE_TICKS * 2;
-        moves++;
-        m[moves].mode = MOVE_MODE_STANGA;
-        m[moves].ticks = ROTATE_TICKS;
-        moves++;
-        ignoreWhiteTicks = IGNORE_WHITE_TICKS;
       }
     }
     else if (S_SS < colorThresholdSpate) { // E alb
       if (dist > distThreshold) { // Are ceva in fata
-        moves = 0;
+        addMove(MOVE_MODE_STANGA  , ROTATE_TICKS    , 5);
+        addMove(MOVE_MODE_FATA ,    ROTATE_TICKS * 2 * DISTANCE_M, 5);
         Serial.println("Spate stanga; il vad in fata;");
-        m[moves].mode = MOVE_MODE_STANGA;
-        m[moves].ticks = ROTATE_TICKS;
-        moves++;
-        m[moves].mode = MOVE_MODE_FATA;
-        m[moves].ticks = ROTATE_TICKS * 2;
-        moves++;
-        ignoreWhiteTicks = IGNORE_WHITE_TICKS;
       }
       else {
-        moves = 0;
+        addMove(MOVE_MODE_DREAPTA  , ROTATE_TICKS    , 5);
+        addMove(MOVE_MODE_FATA     , ROTATE_TICKS * 2 * DISTANCE_M, 5);
         Serial.println("Spate stanga;");
-        m[moves].mode = MOVE_MODE_DREAPTA;
-        m[moves].ticks = ROTATE_TICKS;
-        moves++;
-        m[moves].mode = MOVE_MODE_FATA;
-        m[moves].ticks = ROTATE_TICKS * 2;
-        moves++;
-        ignoreWhiteTicks = IGNORE_WHITE_TICKS;
       }
     }
     else if (S_SD < colorThresholdSpate) { // E alb
       if (dist > distThreshold) { // Are ceva in fata
-        moves = 0;
+        addMove(MOVE_MODE_DREAPTA  , ROTATE_TICKS    , 5);
+        addMove(MOVE_MODE_FATA     , ROTATE_TICKS * 2 * DISTANCE_M, 5);
         Serial.println("Spate dreapta; il vad in fata;");
-        m[moves].mode = MOVE_MODE_DREAPTA;
-        m[moves].ticks = ROTATE_TICKS;
-        moves++;
-        m[moves].mode = MOVE_MODE_FATA;
-        m[moves].ticks = ROTATE_TICKS * 2;
-        moves++;
-        ignoreWhiteTicks = IGNORE_WHITE_TICKS;
       }
       else {
-        moves = 0;
+        addMove(MOVE_MODE_STANGA  , ROTATE_TICKS    , 5);
+        addMove(MOVE_MODE_FATA ,    ROTATE_TICKS * 2 * DISTANCE_M, 5);
         Serial.println("Spate dreapta;");
-        m[moves].mode = MOVE_MODE_STANGA;
-        m[moves].ticks = ROTATE_TICKS;
-        moves++;
-        m[moves].mode = MOVE_MODE_FATA;
-        m[moves].ticks = ROTATE_TICKS * 2;
-        moves++;
-        ignoreWhiteTicks = IGNORE_WHITE_TICKS;
       }
     }
   }
-  else {
-    ignoreWhiteTicks--;
-  }
   
   if (moves > 0) {
+    if (getCurPriority() < 10) {
+      defaultModeTicks = 0;
+    }
     m[moves - 1].ticks--;
-      //Serial.println(m[moves - 1].ticks);
          
     // Cum ne miscam astazi?
     switch(m[moves - 1].mode) {
@@ -237,6 +185,18 @@ void loop()
       case MOVE_MODE_SPATE:
         go(-255, -255);
         break;
+        
+      case MOVE_MODE_STOP:
+        go(0, 0);
+        break;
+        
+      case MOVE_MODE_SPATE_STANGA:
+        go(-255, 150);
+        break;
+        
+      case MOVE_MODE_SPATE_DREAPTA:
+        go(150, -255);
+        break;
     } 
     
     // Termina move-ul
@@ -245,21 +205,45 @@ void loop()
       moves--;
     }
   }
-  else {
+  else {    
+    // Increase ticks for default mode.
+    defaultModeTicks++;
+    
+    // Too many default ticks? Suntem lipiti de el.
+    if (defaultModeTicks > 6000) {
+      int r = random(0, 2);
+      if (0 == 0) {          
+        addMove(MOVE_MODE_STOP, ROTATE_TICKS * 2, 7);
+        addMove(MOVE_MODE_FATA, ROTATE_TICKS * 2, 7);
+        addMove(MOVE_MODE_STOP, ROTATE_TICKS * 2, 7);
+        addMove(MOVE_MODE_FATA, ROTATE_TICKS * 2, 7);
+        addMove(MOVE_MODE_STOP, ROTATE_TICKS * 2, 7);
+        addMove(MOVE_MODE_FATA, ROTATE_TICKS * 2, 7);
+        addMove(MOVE_MODE_STOP, ROTATE_TICKS * 2, 7);
+        addMove(MOVE_MODE_FATA, ROTATE_TICKS * 2, 7);
+      } 
+      else if (r == 1) {     
+        addMove(MOVE_MODE_STANGA, ROTATE_TICKS * 8, 7);
+        addMove(MOVE_MODE_FATA, ROTATE_TICKS * 8, 7);
+      }      
+      else if (r == 2) {     
+        addMove(MOVE_MODE_DREAPTA, ROTATE_TICKS * 8, 7);
+        addMove(MOVE_MODE_FATA, ROTATE_TICKS * 8, 7);
+      }      
+    }
+    
     // Default move -> attack!
-    go(255, 240); 
-    int r = random(0, 2000);
+    go(255, 255); 
+    
+    // Random rotation
+    int r = random(0, 8000);
     if (r == 0) {
+      addMove(MOVE_MODE_STANGA  , ROTATE_TICKS * 4, 10);
       Serial.println("Random stanga.");
-      m[0].mode = MOVE_MODE_STANGA;
-      m[0].ticks = ROTATE_TICKS;
-      moves++;
     } 
     else if (r == 1) {
+      addMove(MOVE_MODE_DREAPTA , ROTATE_TICKS * 4, 10);
       Serial.println("Random dreapta.");
-      m[0].mode = MOVE_MODE_DREAPTA;
-      m[0].ticks = ROTATE_TICKS;
-      moves++;
     }
   }  
 }
